42296945485960e52932752768c86a960f41b4a5
Lucas Guima <lucasguima.felipe@gmail.com>
2021-01-24 19:04:00 -0300

    Handles the parameter type
    Change the logic for the tests to cover all lines

--- src/fizzbuzz/fizzbuzz.js
+++ src/fizzbuzz/fizzbuzz.js
@@ -1,18 +1,22 @@
 function fizzbuzz(number) {
+  if (typeof number !== 'number') {
+    throw new TypeError;
+  }
+
   const divisibleBy3 = number % 3 === 0;
   const divisibleBy5 = number % 5 === 0;
 
-  if ((!divisibleBy3 && !divisibleBy5) || number === 0) {
+  if (number === 0 || (!divisibleBy3 && !divisibleBy5)) {
     return number
   }
-  else if (divisibleBy3 && divisibleBy5) {
-    return 'FizzBuzz';
-  }
-  else if (divisibleBy3) {
+  else if (divisibleBy3 && !divisibleBy5) {
     return 'Fizz';
   }
-  else if (divisibleBy5) {
+  else if (divisibleBy5 && !divisibleBy3) {
     return 'Buzz';
+  }
+  else {
+    return 'FizzBuzz';
   }
 }
 
